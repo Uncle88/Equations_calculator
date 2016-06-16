4aa125ddd0d7c41a7ac6c7b2272d12a91c815587
Uncle88 <andreyproskura88@gmail.com>
2016-06-16 14:19:26 +0300

    Use GridLayout

--- Equations_calculator/Equations_calculator/Resources/Resource.Designer.cs
+++ Equations_calculator/Equations_calculator/Resources/Resource.Designer.cs
@@ -60,38 +60,32 @@ namespace Equations_calculator
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050009
-			public const int MyButton = 2131034121;
-			
-			// aapt resource value: 0x7f050001
-			public const int editText1 = 2131034113;
-			
-			// aapt resource value: 0x7f050004
-			public const int editText2 = 2131034116;
-			
-			// aapt resource value: 0x7f050006
-			public const int editText3 = 2131034118;
-			
 			// aapt resource value: 0x7f050008
-			public const int editText4 = 2131034120;
+			public const int MyButton = 2131034120;
+			
+			// aapt resource value: 0x7f050003
+			public const int editText2 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int editText3 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int editText4 = 2131034119;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int textView2 = 2131034113;
+			
 			// aapt resource value: 0x7f050002
-			public const int textView2 = 2131034114;
+			public const int textView3 = 2131034114;
 			
-			// aapt resource value: 0x7f050003
-			public const int textView3 = 2131034115;
+			// aapt resource value: 0x7f050004
+			public const int textView4 = 2131034116;
 			
-			// aapt resource value: 0x7f050005
-			public const int textView4 = 2131034117;
-			
-			// aapt resource value: 0x7f050007
-			public const int textView5 = 2131034119;
-			
-			// aapt resource value: 0x7f05000a
-			public const int textView6 = 2131034122;
+			// aapt resource value: 0x7f050006
+			public const int textView5 = 2131034118;
 			
 			static Id()
 			{
