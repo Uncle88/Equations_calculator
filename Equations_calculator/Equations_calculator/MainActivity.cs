a60cb64b4140be8672170da1cf50ef252be33d2f
Uncle88 <andreyproskura88@gmail.com>
2016-06-29 23:12:16 +0300

    MyFirstDelivery

--- Equations_calculator/Equations_calculator/MainActivity.cs
+++ Equations_calculator/Equations_calculator/MainActivity.cs
@@ -23,36 +23,21 @@ namespace Equations_calculator
             Tab tab = ActionBar.NewTab();
             tab.SetIcon(Resource.Drawable.Icon);
             tab.TabSelected += (sender, args) =>
-            {
-                 
-            };
+            { };
                 ActionBar.AddTab(tab);
-
-           
-
-            //Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
 
             Spinner sp_a = FindViewById<Spinner>(Resource.Id.spinner1);
             Spinner sp_b = FindViewById<Spinner>(Resource.Id.spinner2);
             Spinner sp_c = FindViewById<Spinner>(Resource.Id.spinner3);
 
-            //sp_a.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
             var adapter = ArrayAdapter.CreateFromResource(
                     this, Resource.Array.numbers_array, Android.Resource.Layout.SimpleSpinnerItem);
 
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             sp_a.Adapter = adapter;
 
-            //sp_b.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
-           // var adapter = ArrayAdapter.CreateFromResource(
-                   // this, Resource.Array.numbers_array, Android.Resource.Layout.SimpleSpinnerItem);
-
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             sp_b.Adapter = adapter;
-
-            //sp_c.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
-           //var adapter = ArrayAdapter.CreateFromResource(
-            //        this, Resource.Array.numbers_array, Android.Resource.Layout.SimpleSpinnerItem);
             
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             sp_c.Adapter = adapter;
@@ -69,26 +54,15 @@ namespace Equations_calculator
             };
         }
 
-        //private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
-        //{
-        //    Spinner spinner = (Spinner)sender;
-        //}
-
         public string Diskriminant(double a, double b, double c)
         {
             var tmpResult = b * b - 4 * a * c;
             if (tmpResult > 0)
             {
-                //double X1 = (-b + Math.Sqrt(tmpResult)) / (2 * a);
-                //double X2 = (-b - Math.Sqrt(tmpResult)) / (2 * a);
-
                 return "D>0 - The equation has two radicals";
             }
             else if (tmpResult == 0)
             {
-                //double X1= (-b) / (2 * a);
-                //double X2= (-b) / (2 * a);
-
                 return "D=0 - The equation has two equal radicals";
             }
             else if (tmpResult < 0)
